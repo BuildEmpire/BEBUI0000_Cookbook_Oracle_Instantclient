5d9899d482d820f6e49841f8513834ce8d0ccc0c
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-06-16 19:24:22 +0100

    Fixed another reference error.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ default['cookbook_oracle_instantclient']['basic_rpm'] = 'oracle-instantclient12.
 default['cookbook_oracle_instantclient']['basic_zip'] = 'instantclient-basic-linux.x64-12.1.0.1.0.zip'
 default['cookbook_oracle_instantclient']['sdk_zip'] = 'instantclient-sdk-linux.x64-12.1.0.1.0.zip'
 default['cookbook_oracle_instantclient']['folder_name'] = 'instantclient_12_1'
-default['cookbook_oracle_instantclient']['version'] = default['oracle_instantclient']['basic_rpm'].split('-')[1].sub(/instantclient/, '')
+default['cookbook_oracle_instantclient']['version'] = default['cookbook_oracle_instantclient']['basic_rpm'].split('-')[1].sub(/instantclient/, '')
