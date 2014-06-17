f2ca417f3d56324e45a6d6c33798b341b7774ecf
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-06-17 22:04:45 +0100

    Updated the cookbook to change the way we install the oracle stuff.

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,24 +12,23 @@ cookbook_file node['cookbook_oracle_instantclient']['sdk_zip'] do
   action :create_if_missing
 end
 
-directory "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['version']}/client64" do
+directory "/usr/lib/oracle" do
   action :create
   recursive true
 end
 
 execute "Unzip basic into directory" do
-  command "unzip -o #{File.join(Chef::Config[:file_cache_path], node['cookbook_oracle_instantclient']['basic_zip'])} -d /tmp"
+  command "unzip -o #{File.join(Chef::Config[:file_cache_path], node['cookbook_oracle_instantclient']['basic_zip'])} -d /usr/lib/oracle/"
 end
 
 execute "Unzip sdk into directory" do
-  command "unzip -o #{File.join(Chef::Config[:file_cache_path], node['cookbook_oracle_instantclient']['sdk_zip'])} -d /tmp"
+  command "unzip -o #{File.join(Chef::Config[:file_cache_path], node['cookbook_oracle_instantclient']['sdk_zip'])} -d /usr/lib/oracle/"
 end
 
-execute "Move the files" do
-  command "mv /tmp/#{node['cookbook_oracle_instantclient']['folder_name']} /usr/lib/oracle/#{node['cookbook_oracle_instantclient']['version']}/client64/lib"
-  not_if do FileTest.directory?("/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['version']}/client64/lib") end
+link "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['folder_name']}/lib/libclntsh.so" do
+  to "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['folder_name']}/lib/libclntsh.so.#{node['cookbook_oracle_instantclient']['version']}"
 end
 
-link "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['version']}/client64/lib/libclntsh.so" do
-  to "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['version']}/client64/lib/libclntsh.so.#{node['cookbook_oracle_instantclient']['version']}"
+link "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['folder_name']}/lib/libocci.so" do
+  to "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['folder_name']}/lib/libocci.so.#{node['cookbook_oracle_instantclient']['version']}"
 end
