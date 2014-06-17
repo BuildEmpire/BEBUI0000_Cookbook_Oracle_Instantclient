adba19a08a7a8b4d7e46720bee0f3ff06535d388
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-06-17 23:24:14 +0100

    Added magic shell dependency.

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,12 @@ maintainer_email 'fred.thompson@buildempire.co.uk'
 license          'Apache 2.0'
 description      'Cookbook Oracle Instantclient'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.5'
+version          '0.0.6'
 
 recipe 'cookbook_oracle_instantclient', 'Cookbook Oracle Instantclient.'
 
 %w{ ubuntu }.each do |os|
   supports os
 end
+
+depends 'magic_shell'
