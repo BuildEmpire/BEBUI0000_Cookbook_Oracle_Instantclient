164b71db6d8af5b8fa0e7b7637e3aaeef819a5e9
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-06-17 22:27:02 +0100

    Fixed a path.

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,10 +25,10 @@ execute "Unzip sdk into directory" do
   command "unzip -o #{File.join(Chef::Config[:file_cache_path], node['cookbook_oracle_instantclient']['sdk_zip'])} -d /usr/lib/oracle/"
 end
 
-link "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['folder_name']}/lib/libclntsh.so" do
-  to "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['folder_name']}/lib/libclntsh.so.#{node['cookbook_oracle_instantclient']['version']}"
+link "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['folder_name']}/libclntsh.so" do
+  to "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['folder_name']}/libclntsh.so.#{node['cookbook_oracle_instantclient']['version']}"
 end
 
-link "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['folder_name']}/lib/libocci.so" do
-  to "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['folder_name']}/lib/libocci.so.#{node['cookbook_oracle_instantclient']['version']}"
+link "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['folder_name']}/libocci.so" do
+  to "/usr/lib/oracle/#{node['cookbook_oracle_instantclient']['folder_name']}/libocci.so.#{node['cookbook_oracle_instantclient']['version']}"
 end
